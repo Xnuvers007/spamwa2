dd57374c533d02cbdc1a89919a6e473eb1fd0ecf
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2021-12-22 19:58:35 +0700

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -18,6 +18,8 @@ done
 #install dependencies pip
 	pip3 install pyautogui
 	pip3 install MouseInfo
+	xhost + #activate python3-xlib
+		#to solve bug raise error.DisplayConnectionError(self.display_name, r.reason) Xlib.error.DisplayConnectionError: Can't connect to display ":1.0": b'No protocol specified\n'
 
 chmod +x start.sh
 ./start.sh
