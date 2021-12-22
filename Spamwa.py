958c17076b89bc8b6e9bfc6bd32901ab5e9ae806
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2021-12-22 20:12:36 +0700

    Update Spamwa.py

--- Spamwa.py
+++ Spamwa.py
@@ -2,8 +2,15 @@ import pyautogui
 import time
 import mouseinfo
 import os
-os.environ['DISPLAY'] = ':1.0' #Linux / debian
-os.environ['DISPLAY'] = ':0' #windows
+from sys import platform
+
+if platform=="linux" or platform=="linux2":
+   os.environ['DISPLAY'] = ':1.0' #Linux / debian
+elif platform=="win32":
+   s.environ['DISPLAY'] = ':0' #windows
+else:
+   print("Cannot detect this device")
+
 os.environ['XAUTHORITY']='/run/user/1000/gdm/Xauthority'
 input("Enter To Start")
 time.sleep(1)
